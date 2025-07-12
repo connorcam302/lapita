1673d0762c5236ae1510bb2de920c7ea31f5228f
Connor <connorcam302@gmail.com>
2025-07-13 03:00:26 +0800

    removed edge

--- src/routes/api/grandprix/[id]/+server.ts
+++ src/routes/api/grandprix/[id]/+server.ts
@@ -3,10 +3,6 @@ import type { RequestHandler } from './$types';
 import { getRaceResultsByGpId } from '$lib/server/serverUtils';
 import type { Config } from '@sveltejs/adapter-vercel';
 
-export const config: Config = {
-	runtime: 'edge'
-};
-
 export const GET: RequestHandler = async ({ params }) => {
 	return json(await getRaceResultsByGpId(params.id));
 };
