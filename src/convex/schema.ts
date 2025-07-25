1d19055ab928c5cba761a9637aeff62237f9d972
Connor <connorcam302@gmail.com>
2025-07-25 20:06:18 +0800

    schema updates

--- src/convex/schema.ts
+++ src/convex/schema.ts
@@ -3,77 +3,56 @@ import { v } from 'convex/values';
 
 export default defineSchema({
 	users: defineTable({
-		name: v.optional(v.string()),
-		id: v.optional(v.number())
-	})
-		.index('name', ['name'])
-		.index('id', ['id']),
+		name: v.string()
+	}).index('name', ['name']),
+
 	tracks: defineTable({
-		name: v.optional(v.string()),
-		id: v.optional(v.string()),
-		img: v.optional(v.string())
-	})
-		.index('name', ['name'])
-		.index('id', ['id']),
+		name: v.string(),
+		img: v.string()
+	}).index('name', ['name']),
+
 	characters: defineTable({
 		name: v.optional(v.string()),
-		id: v.optional(v.string()),
 		img: v.optional(v.string())
-	})
-		.index('name', ['name'])
-		.index('id', ['id']),
+	}).index('name', ['name']),
+
 	karts: defineTable({
-		name: v.optional(v.string()),
-		id: v.optional(v.string()),
-		img: v.optional(v.string())
-	})
-		.index('name', ['name'])
-		.index('id', ['id']),
+		name: v.string(),
+		img: v.string()
+	}).index('name', ['name']),
+
 	grandPrix: defineTable({
-		order: v.optional(v.number()),
-		id: v.optional(v.number()),
-		participants: v.optional(v.array(v.union(v.id('users'), v.number()))),
-		finish_time: v.optional(v.string())
-	})
-		.index('order', ['order'])
-		.index('id', ['id']),
+		order: v.number(),
+		participants: v.array(v.id('users'))
+	}).index('order', ['order']),
+
 	grandPrixStandings: defineTable({
-		grandPrixId: v.optional(v.id('grandPrix')),
-		userId: v.optional(v.id('users')),
-		points: v.optional(v.number())
+		grandPrixId: v.id('grandPrix'),
+		userId: v.id('users'),
+		points: v.number()
 	})
 		.index('grandPrixId', ['grandPrixId'])
 		.index('points', ['points'])
 		.index('userId', ['userId'])
 		.index('userIdAndGrandPrixId', ['userId', 'grandPrixId']),
+
 	races: defineTable({
-		order: v.optional(v.number()),
-		id: v.optional(v.number()),
-		finish_time: v.optional(v.string()),
-		grandPrixId: v.optional(v.id('grandPrix')),
-		grand_prix_id: v.optional(v.number()),
-		trackStartId: v.optional(v.id('tracks')),
-		track_start_id: v.optional(v.string()),
-		trackEndId: v.optional(v.id('tracks')), // Now pure v.id.
-		track_end_id: v.optional(v.string()), // Now pure v.id
-		transition: v.optional(v.boolean())
+		order: v.number(),
+		grandPrixId: v.id('grandPrix'),
+		trackStartId: v.id('tracks'),
+		trackEndId: v.id('tracks'), // Now pure v.id.
+		transition: v.boolean()
 	})
 		.index('grandPrixId', ['grandPrixId'])
-		.index('trackId', ['trackStartId', 'trackEndId'])
-		.index('id', ['id']),
+		.index('trackId', ['trackStartId', 'trackEndId']),
+
 	results: defineTable({
-		id: v.optional(v.number()),
 		position: v.optional(v.union(v.null(), v.number())),
-		userId: v.optional(v.id('users')),
-		user_id: v.optional(v.number()),
-		raceId: v.optional(v.id('races')),
-		race_id: v.optional(v.number()),
-		characterId: v.optional(v.id('characters')), // Now pure v.id
-		character_id: v.optional(v.union(v.string(), v.null())),
-		kartId: v.optional(v.id('karts')), // Now pure v.id
-		kart_id: v.optional(v.union(v.string(), v.null()))
+		userId: v.id('users'),
+		raceId: v.id('races'),
+		characterId: v.optional(v.id('characters')),
+		kartId: v.optional(v.id('karts'))
 	})
 		.index('raceId', ['raceId'])
 		.index('raceIdAndUserId', ['raceId', 'userId'])
-		.index('id', ['id'])
 });
